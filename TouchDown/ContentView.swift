cf253b3cfbb531a8e8ceda9382ba999f5e3e4e26
ko-suke24 <kosuke0322@gmail.com>
2022-03-01 17:12:34 +0900

    Initial Commit

---
+++ TouchDown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by 佐藤航輔 on 2022/03/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 佐藤航輔 on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
