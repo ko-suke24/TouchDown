6cfaef2820f8f0fdca71736134e5293533500dde
ko-suke24 <kosuke0322@gmail.com>
2022-03-01 17:46:16 +0900

    FooterView

---
+++ TouchDown/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by 佐藤航輔 on 2022/03/01.
+//
+
+import Foundation
---
+++ TouchDown/View/Home/FooterView.swift
@@ -0,0 +1,20 @@
+//
+//  FooterView.swift
+//  TouchDown
+//
+//  Created by 佐藤航輔 on 2022/03/01.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
