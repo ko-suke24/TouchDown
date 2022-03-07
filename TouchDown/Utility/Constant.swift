7ffa60e793b3ca29e07c17387860ea6105f8cdc1
ko-suke24 <kosuke0322@gmail.com>
2022-03-07 15:36:52 +0900

    FooterView created

--- TouchDown/Utility/Constant.swift
+++ TouchDown/Utility/Constant.swift
@@ -5,4 +5,8 @@
 //  Created by 佐藤航輔 on 2022/03/01.
 //
 
-import Foundation
+import SwiftUI
+
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
--- TouchDown/View/Home/FooterView.swift
+++ TouchDown/View/Home/FooterView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct FooterView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    VStack(alignment: .center, spacing: 10) {
+      Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .layoutPriority(2)
+
+      Image("logo-lineal")
+        .renderingMode(.template)
+        .foregroundColor(.gray)
+        .layoutPriority(0)
+
+      Text("Copyright © Robert Petras/Arthur Neves\nAll rights reserved")
+        .font(.footnote)
+        .fontWeight(.bold)
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .layoutPriority(1)
+    } //: VStack
+  }
 }
 
-struct FooterView_Previews: PreviewProvider {
-    static var previews: some View {
-        FooterView()
-    }
+struct Footeriew_Previews: PreviewProvider {
+  static var previews: some View {
+    FooterView()
+      .previewLayout(.sizeThatFits)
+      .background(colorBackground)
+  }
 }
